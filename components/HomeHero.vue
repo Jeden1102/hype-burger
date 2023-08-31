ec1d0c4325a495b62e597daaa329e4b5a6562414
Jeden1102 <bum123@o2.pl>
2023-08-31 21:03:05 +0200

    master. Heading added + home hero start.

---
+++ components/HomeHero.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="home-hero">
+    <div class="home-hero__image-left"></div>
+    <div class="home-hero__main"></div>
+    <div class="home-hero__image-right"></div>
+  </div>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style scoped>
+</style>
